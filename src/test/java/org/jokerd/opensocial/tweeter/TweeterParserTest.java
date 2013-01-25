32eca023236f96af467b734d99f22d9f1592190c
arkub <slauriere@ubimix.com>
2013-01-25 16:46:06 +0100

    initial revision

---
+++ src/test/java/org/jokerd/opensocial/tweeter/TweeterParserTest.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package org.jokerd.opensocial.tweeter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.jokerd.opensocial.tweeter.model.TweetParser;
+
+/**
+ * @author kotelnikov
+ */
+public class TweeterParserTest extends TestCase {
+
+    /**
+     * @param name
+     */
+    public TweeterParserTest(String name) {
+        super(name);
+    }
+
+    protected void println(String string) {
+        System.out.println(string);
+    }
+
+    public void test() throws Exception {
+        test("a", "TEXT:'a'");
+        test("@a", "USER:'@a'");
+        test("#a", "HASH:'#a'");
+        test("http://www.foo.bar", "URL:'http://www.foo.bar'");
+        test("a #hash b", "TEXT:'a '", "HASH:'#hash'", "TEXT:' b'");
+        test("a @user b", "TEXT:'a '", "USER:'@user'", "TEXT:' b'");
+        test(
+            "before http://www.foo.bar after",
+            "TEXT:'before '",
+            "URL:'http://www.foo.bar'",
+            "TEXT:' after'");
+        test(
+            "before .http://www.foo.bar. after",
+            "TEXT:'before .'",
+            "URL:'http://www.foo.bar'",
+            "TEXT:'. after'");
+        test(
+            "before ;http://www.foo.bar; after",
+            "TEXT:'before ;'",
+            "URL:'http://www.foo.bar'",
+            "TEXT:'; after'");
+        test(
+            "http://www.foo.bar @user #hash",
+            "URL:'http://www.foo.bar'",
+            "TEXT:' '",
+            "USER:'@user'",
+            "TEXT:' '",
+            "HASH:'#hash'");
+    }
+
+    private void test(String str, String... control) {
+        final List<String> list = new ArrayList<String>();
+        TweetParser parser = new TweetParser();
+        println("==========================");
+        parser.parse(str, new TweetParser.IListener() {
+
+            private void append(String string) {
+                println(string);
+                list.add(string);
+            }
+
+            public void onHashtag(String value) {
+                append("HASH:'" + value + "'");
+            }
+
+            public void onText(String value) {
+                append("TEXT:'" + value + "'");
+            }
+
+            public void onUrl(String value) {
+                append("URL:'" + value + "'");
+            }
+
+            public void onUserReference(String value) {
+                append("USER:'" + value + "'");
+            }
+        });
+        assertEquals(control.length, list.size());
+        int i = 0;
+        for (String s : control) {
+            assertEquals(s, list.get(i++));
+        }
+    }
+}
